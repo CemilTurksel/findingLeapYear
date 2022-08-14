8cdfdaf38356066a3ad6fa5e046ee5cca260f74c
Cemil Türksel <86201517+CemilTurksel@users.noreply.github.com>
2022-08-15 00:34:34 +0300

    Add files via upload

---
+++ findingLeapYear/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        int year;
+        System.out.print("Enter a year: ");
+        year = scanner.nextInt();
+
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                System.out.println(year + " is a leap year");
+            } else {
+                System.out.println(year + " is not a leap year");
+            }
+        } else {
+            if (year % 4 == 0) {
+                System.out.println(year + " is a leap year");
+            } else {
+                System.out.println(year + " is not a leap year");
+            }
+        }
+
+    }
+
+}
